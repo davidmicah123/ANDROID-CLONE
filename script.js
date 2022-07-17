620b50fb54a7183d2a4b3c9ab1343c86411c7a6b
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-07-17 23:23:39 +0100

    Adding cancel popup functionality

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ popUp.addEventListener('mouseout',
         popUp.style.backgroundColor = "rgba(255, 255, 255, 0.87)"
     }
 );
+const closePopUp = document.querySelector('.close_popup');
+closePopUp.addEventListener('click', 
+    () => {
+        console.log(1)
+        popUp.style.animationName = `slideOut`;
+        if (popUp.style.animationName === `slideOut`) {
+            popUp.style.opacity = 0;
+            popUp.classList.add('hidden');
+        }
+    }
+);
