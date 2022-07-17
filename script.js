1f73b8a883f54c2d8df84a665bb77afb715f0826
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-07-17 23:21:37 +0100

    Adding popup cancel functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+const popUp = document.querySelector('.fixed_container');
+popUp.addEventListener('mouseover',
+    () => {
+        popUp.style.backgroundColor = "rgba(255, 255, 255, 1)"
+    }
+); 
